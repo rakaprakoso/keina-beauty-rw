4e8d24c5c6843419ed791b689fcaaa781dc54f8b
rakaprakoso <62010062+rakaprakoso@users.noreply.github.com>
2021-09-04 16:36:14 +0700

    CSS Aset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <title>Keina Beauty</title> --}}
     <meta name="theme-color" content="#2a3f2b"/>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <link rel="stylesheet" href="{{ mix('/css/appClient.css') }}">
+    <link rel="stylesheet" href="{{ asset(mix('/css/app.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('/css/appClient.css')) }}">
     <link rel="shortcut icon" href="/assets/green-white.png" type="image/x-icon" />
     {!! SEO::generate() !!}
 </head>
--- resources/views/index_admin.blade.php
+++ resources/views/index_admin.blade.php
@@ -7,8 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Keina Beauty</title>
     <meta name="theme-color" content="#2a3f2b"/>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <link rel="stylesheet" href="{{ mix('/css/appAdmin.css') }}">
+    <link rel="stylesheet" href="{{ asset(mix('/css/app.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('/css/appAdmin.css')) }}">
 </head>
 <body>
     <div id="root">
